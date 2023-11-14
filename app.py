e60f00e464eeac914d4c73dcde178167435af612
Srujan-rai <raisrujan@gmail.com>
2023-11-14 14:35:45 +0530

    requirement edit

--- app.py
+++ app.py
@@ -28,5 +28,9 @@ def index():
     return render_template("index.html", password=password)
 
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    # Use Gunicorn as the web server
+    import os
+    host = '0.0.0.0'
+    port = int(os.environ.get('PORT', 8000))  # Use the PORT environment variable provided by Render
+    app.run(debug=False, host=host, port=port)
