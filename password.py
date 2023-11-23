aadc6e8996371350f72afe0d8e50c92b921374aa
Srujan-rai <raisrujan@gmail.com>
2023-11-23 14:35:56 +0530

    internship day 1 programs added

--- password.py
+++ password.py
@@ -1,10 +1,19 @@
 import random
+import string
 
-lower="abcdefghijklmnopqrstuvwxyz"
-higher="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-numbers="123456789"
-symbols="!@$%*;/,._-"
-all=lower+higher+numbers+symbols
-length=16
-password="".join(random.sample(all,length))
+lowercase = string.ascii_lowercase
+# print(lowercase)
+uppercase = string.ascii_uppercase
+digit = string.digits
+specialCharacters = string.punctuation
+
+
+length = int(input("Enter the length of the password: "))
+
+all = lowercase + uppercase + digit + specialCharacters
+
+# print(all)
+
+password = ''.join(random.sample(all, length))
+
 print(password)
